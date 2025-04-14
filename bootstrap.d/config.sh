2f88d551810b2b8c78cfa5967cfcdb3376446871
Bertrand Lanson <bertrand.lanson@protonmail.com>
2025-04-14 10:11:38 +0200

    feat: make hashicorp endpoints configurable through config.sh
    also, add a vault_login_ldap function

--- bootstrap.d/config.sh
+++ bootstrap.d/config.sh
@@ -2,3 +2,8 @@
 
 # this is used to allow using hardware security keys for ssh
 USE_GNUPG_FOR_SSH=true
+
+# configure endpoints for hashicorp clusters
+CONFIG_VAULT_ADDR=""
+CONFIG_CONSUL_HTTP_ADDR=""
+CONFIG_NOMAD_ADDR=""

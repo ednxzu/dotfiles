fd697ad55f129453f90dc63fc8ff840f454f5a25
Bertrand Lanson <bertrand.lanson@protonmail.com>
2025-09-27 23:23:46 +0200

    fix: adjust vscodium extensions and sync service

--- .config/dotfiles/install-extensions.sh
+++ .config/dotfiles/install-extensions.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+CODIUM=/usr/bin/codium
 EXT_FILE="$HOME/.config/VSCodium/extensions.list"
 
 if [[ ! -f "$EXT_FILE" ]]; then
@@ -8,12 +9,12 @@ if [[ ! -f "$EXT_FILE" ]]; then
   exit 0
 fi
 
-INSTALLED=$(codium --list-extensions)
+INSTALLED=$($CODIUM --list-extensions)
 
 while IFS= read -r ext; do
   if [[ -n "$ext" && ! "$INSTALLED" =~ "$ext" ]]; then
     echo "[+] Installing $ext"
-    codium --install-extension "$ext"
+    $CODIUM --install-extension "$ext"
   fi
 done < "$EXT_FILE"
 

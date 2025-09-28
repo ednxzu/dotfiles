50ab58963c6bc5195b00829f89c92ded9354398c
Bertrand Lanson <bertrand.lanson@protonmail.com>
2025-09-28 15:54:15 +0200

    feat: adjust config file

--- bootstrap.d/config.sh
+++ bootstrap.d/config.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-# this is used to allow using hardware security keys for ssh
-USE_GNUPG_FOR_SSH=true
-
 # configure endpoints for hashicorp clusters
 CONFIG_VAULT_ADDR=""
 CONFIG_CONSUL_HTTP_ADDR=""

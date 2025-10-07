b167b1a6393dc89a1fbf139d5bf4a45acad876b6
Bertrand Lanson <bertrand.lanson@protonmail.com>
2025-10-08 00:40:41 +0200

    fix: persists compose key on hotplug

--- .config/bspwm/scripts/init-input.sh
+++ .config/bspwm/scripts/init-input.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 CURSOR="left_ptr"
-COMPOSE_KEY="ralt"
 
 # Set cursor
 xsetroot -cursor_name "$CURSOR"
-
-# Reset and set desired XKB options
-setxkbmap -option  # clears all
-setxkbmap -option compose:"$COMPOSE_KEY"

19a6e17b7cc02cc318a45d660ec1c1141e047cf2
Bertrand Lanson <bertrand.lanson@protonmail.com>
2026-02-25 21:08:01 +0100

    feat: more nvim stuff

--- .config/nvim/lua/plugins/explorer.lua
+++ .config/nvim/lua/plugins/explorer.lua
@@ -11,6 +11,19 @@ return {
           explorer = {
             hidden = true,
             ignored = true,
+            layout = {
+              layout = {
+                backdrop = false,
+                width = 40,
+                min_width = 40,
+                height = 0,
+                position = "left",
+                border = "none",
+                box = "vertical",
+                { win = "list", border = "none" },
+                { win = "preview", title = "{preview}", height = 0.4, border = "top" },
+              },
+            },
             win = {
               list = {
                 keys = {
---
+++ .config/nvim/lua/plugins/jinja.lua
@@ -0,0 +1,7 @@
+return {
+  -- Jinja2 syntax highlighting (no treesitter parser exists)
+  {
+    "lepture/vim-jinja",
+    ft = { "jinja", "jinja2", "htmljinja" },
+  },
+}
